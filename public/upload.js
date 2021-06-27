e28ab4a91817943f0b8c96ab8e1c03b1733f2815
Joel del Castillo <63157244+joeldelcastillo@users.noreply.github.com>
2021-06-26 19:17:30 -0500

    Get ip data

--- public/upload.js
+++ public/upload.js
@@ -1,13 +1,28 @@
 const btn = document.getElementById("btn1");
 
 var input = $("#input");
+
 const timeNow = new Date();
+
+var IP;
+let apiURL = "https://ipinfo.io/json";
+
+const getIP = async () => {
+  let IPData = await fetch(apiURL).then((j) => j.json());
+  console.log(IPData.ip);
+  IP = IPData.ip;
+  // {ip: "167.99.230.64", city: "North Bergen",  …}
+};
+
+getIP();
 
 function writeData() {
   alert("to server");
+
   firebase.database().ref("User").set({
     name: input.val(),
-    time: timeNow.toUTCString(),
+    time: timeNow.toDateString(),
+    ip: IP,
   });
 }
 
