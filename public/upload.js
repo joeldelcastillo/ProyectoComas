551918b3c6c325304967a42a0c685247325faf6d
Joel del Castillo <63157244+joeldelcastillo@users.noreply.github.com>
2021-07-10 20:02:13 -0500

    structured database

--- public/upload.js
+++ public/upload.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("btn1");
 var input = $("#input");
 const timeNow = new Date();
 var IP;
+var numIp;
 let apiURL = "https://ipinfo.io/json";
 var isFirstTime;
 
@@ -9,20 +10,33 @@ const getIP = async () => {
   let IPData = await fetch(apiURL).then((j) => j.json());
   console.log(IPData);
   IP = IPData;
+  var str = IP.ip.split(".");
+  var final = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i < str.length - 1) {
+      final = final + str[i] + "-";
+    } else {
+      final = final + str[i];
+    }
+  }
+  numIp = final;
+  console.log(numIp);
   // {ip: "167.99.230.64", city: "North Bergen",  …}
 };
 
 getIP();
 
 function firstTime() {
-  const dbRef = firebase.database().ref(`${timeNow.toDateString()}/178958`);
+  const dbRef = firebase.database().ref(`${timeNow.toDateString()}/${numIp}`);
   dbRef
     .get()
     .then((snapshot) => {
       if (snapshot.exists()) {
         console.log(snapshot.val());
+        isFirstTime = false;
       } else {
         console.log("No data available");
+        isFirstTime = true;
       }
     })
     .catch((error) => {
@@ -30,12 +44,11 @@ function firstTime() {
     });
 }
 
-isFirstTime = firstTime();
-console.log(isFirstTime);
+setTimeout(firstTime, 1000);
 
 function addUses() {
   const updates = {};
-  updates[`${timeNow.toDateString()}/178958/used`] =
+  updates[`${timeNow.toDateString()}/${numIp}/used`] =
     firebase.database.ServerValue.increment(1);
   // updates[`user-posts/${key}/stars/${uid}`] = true;
   firebase.database().ref().update(updates);
@@ -45,11 +58,9 @@ function writeData() {
   alert("to server");
 
   if (isFirstTime == true) {
-    firebase.database().ref(`${timeNow.toDateString()}/178958`).set({
-      // ip: IP.ip,
-      // city: IP.city,
-      // country: IP.country,
-      country: "ecuador",
+    firebase.database().ref(`${timeNow.toDateString()}/${numIp}`).set({
+      city: IP.city,
+      country: IP.country,
       name: input.val(),
       used: 1,
     });
